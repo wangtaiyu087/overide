7bed417f28938c6fc26bd217c1fa43faffa7b685
Li <gjo4us06c961115@gmail.com>
2019-11-28 20:06:03 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Duck.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public abstract class Duck
+    {
+        public string Name { get; set; } ="duck";
+        public abstract void Swim();
+
+        public void Walk()
+        {
+
+            Console.WriteLine("cc");
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            Toyduck toyduck = new Toyduck();
+
+            toyduck.Walk();
+            toyduck.Swim();
+
+            Console.ReadKey();
+
+        }
+    }
+}
