c0ae7af86ef8a697790ce36e1ce68cf52e6ca803
20191125 <20191125@20191125-PC>
2019-12-04 15:17:38 +0800

    空白

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,8 +10,7 @@ namespace ConsoleApp1
     {
          static void Main(string[] args)
         {
-            Toyduck toyduck = new Toyduck();
-
+            Toyduck toyduck = new Toyduck();  
             toyduck.Walk();
             toyduck.Swim();
 
